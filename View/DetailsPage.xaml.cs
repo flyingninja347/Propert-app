04e6d4dc7f51cfc0cd5f9589d3bc30b85263b242
flyingninja347 <148359509+flyingninja347@users.noreply.github.com>
2025-09-05 21:35:14 +0300

    Update DetailsPage.xaml.cs

--- View/DetailsPage.xaml.cs
+++ View/DetailsPage.xaml.cs
@@ -1,5 +1,6 @@
 using PropertyApp.ViewModel;
 
+
 namespace PropertApp.View;
 
 public partial class DetailsPage : ContentPage
@@ -13,6 +14,49 @@ public partial class DetailsPage : ContentPage
 
 		this.BindingContext = viewModel;
 
-    }
+		SetViewPositions();
 
+		Loaded += (s, e) =>
+		{
+			FadeAndScale(detailsBtn);
+			RotateView(detailsBtn);
+			FadeAndTranslate(imagesView);
+
+			Task.Delay(millisecondsDelay: 500);
+
+            FadeAndTranslate(popView, fadelength:1000, translateLength:1000);
+            FadeAndTranslate(addressView, fadelength: 1000, translateLength: 1000);
+            FadeAndScale(buyBtn, fadelength: 1000, scaleLength: 1000);
+        };
+    }
+	private void SetViewPositions()
+	{
+		detailsBtn.Opacity = 0;
+		detailsBtn.Scale = 0.2;
+		detailsBtn.Rotation = 300;
+
+		imagesView.TranslationX = 300;
+		imagesView.Opacity = 0;
+
+        addressView.TranslationX = addressView.TranslationY = -30;
+        addressView.Opacity = 0;
+
+        buyBtn.Opacity = 0;
+		buyBtn.Scale = 0.2;
+
+		popView.TranslationY = 300;
+		popView.Opacity = 0.5;
+	}
+	private void FadeAndTranslate(VisualElement view, uint fadelength = 1000, uint translateLength = 1500)
+	{
+		view.FadeTo(opacity:1, fadelength, Easing.SinInOut);
+		view.TranslateTo(x:0, y:0 ,translateLength,Easing.SinInOut);
+
+	}
+	private void FadeAndScale(VisualElement view,uint fadelength = 1000, uint scaleLength =1500)
+	{
+		view.FadeTo(opacity: 1, fadelength, Easing.SinInOut);
+		view.ScaleTo(scale:1, scaleLength,Easing.SinInOut);
+	}
+	private void RotateView(VisualElement view) => view.RotateTo(rotation:0, length:1500, Easing.SinInOut);
 }
